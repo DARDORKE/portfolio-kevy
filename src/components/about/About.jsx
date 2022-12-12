bb1ff5e7b73d016c4199476a68b99dc23af335e6
Kévy DARDOR <kevy.dardor@gmail.com>
2022-12-12 14:50:36 +0100

    Création des composants React

---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return 
+import './about.css'
+  (
+    <div>About</div>
+  )
+}
+import './about.css'
+
+export default About
